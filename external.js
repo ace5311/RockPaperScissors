8bb2756aa41d75ba3bb5fa80f6aedc60c1651321
Andrew <erneandrew@gmail.com>
2022-09-23 12:10:14 -0700

    Modify files and add images

--- external.js
+++ external.js
@@ -1,57 +1,62 @@
+let computerSelection = 0;
+let playerSelection = 0;
+let i=0; let j=0;
+const buttons = document.querySelectorAll('.btn');
 
-let playerSelection = "rock"
+const player = document.querySelector('#player-score');
+const computer = document.querySelector('#computer-score');
+const output = document.querySelector('#output');
+
+player.textContent = `Player Score: ${i}`;
+computer.textContent = `Computer Score: ${j}`;
+output.textContent = `How Dare You Challenge Me!`;
+
+buttons.forEach(button => {button.addEventListener('click',()=>{
+    let playerChoiceID = button.id;
+    if (playerChoiceID === "rock") playerSelection = 0;
+    if (playerChoiceID === "paper") playerSelection = 1;
+    if (playerChoiceID === "scissors") playerSelection = 2;
+    computerSelection = getComputerChoice();
+    console.log(playerSelection);
+    console.log(computerSelection);
+    game();
+})
+});
 
 function getComputerChoice(){
-    var x = Math.floor(Math.random() * 3);
-    if (x == 0) {
-        return "rock";
-    }
-    else if (x == 1) {
-        return "scissors";
-    }
-    else if (x == 2) {
-        return "paper";
-    }
+    let randomNum = Math.floor(Math.random() * 3);
+    return randomNum;
 }
 
-
 function playRound(playerSelection, computerSelection) {
-    
-    if (playerSelection == computerSelection){
-        return "Draw";
+    //array is results from different combinations. 0=draw, 1=lose, 2=win.
+    let outcomes = [[0, 1, 2],
+                    [2, 0, 1],
+                    [1, 2, 0]];
+    let result = outcomes[playerSelection][computerSelection];
+    console.log(result);
+    if (result == 0) {
+        output.textContent = `Nice Try! We Drew!`
+        }
+    if (result == 1) {
+        output.textContent = `You Lose! Loser!`
+        j++;
     }
-    else if ((playerSelection == "scissors" && computerSelection == "rock") || (playerSelection == "rock" && computerSelection == "paper") || (playerSelection == "paper" && computerSelection == "scissors")){
-        return "Lost";
-    }
-    else if ((playerSelection == "scissors" && computerSelection == "paper") || (playerSelection == "rock" && computerSelection == "scissors") || (playerSelection == "paper" && computerSelection == "rock")){
-        return "Won";
+    if (result == 2) {
+        output.textContent = `You Won! But You Won't Again!`
+        i++;
     }
 }
 
 function game(){
-    let i=0;let j=0;
-    while (i<5 || j<5){
-        playerSelection=prompt("What's your choice? Rock, Paper, or Scissors?").toLowerCase();
-        let computerSelection = getComputerChoice();
-        let roundResult = playRound(playerSelection, computerSelection);
-        if (roundResult == "Draw"){
-            console.log("Draw! Try again!")
-        }
-        else if (roundResult == "Won"){
-            i++;
-            console.log("Won this round");
-        }
-        else if (roundResult== "Lost"){
-            j++;
-            console.log("Lost this round");
-        }
+    playRound(playerSelection, computerSelection);
+    player.textContent = `Player Score: ${i}`;
+    computer.textContent = `Computer Score: ${j}`;
+    if (i==5){
+        output.textContent = `OH NO! YOU WON!!! YOU MUST HAVE CHEATED!`;
     }
-    if (i=5){
-        console.log("You Won!");
-    }
-    else if (j=5){
-        console.log("You Lost! Loser!");
+    if (j==5){
+        output.textContent = `HAHAHAHA YOU LOST! YOU STINK!`
     }
 }
 
-game();
