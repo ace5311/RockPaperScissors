457de607b355f1725d39cf38e7d3371b5c14a321
Andrew <erneandrew@gmail.com>
2022-09-30 09:41:57 -0700

    Modify files to include a different background and add a reset

--- external.js
+++ external.js
@@ -1,25 +1,31 @@
 let computerSelection = 0;
 let playerSelection = 0;
-let i=0; let j=0;
+let i=0; let j=0; let firstRound=1;
 const buttons = document.querySelectorAll('.btn');
-
+const resetBtn = document.querySelector('#resetBtn');
 const player = document.querySelector('#player-score');
 const computer = document.querySelector('#computer-score');
 const output = document.querySelector('#output');
 
 player.textContent = `Player Score: ${i}`;
 computer.textContent = `Computer Score: ${j}`;
-output.textContent = `How Dare You Challenge Me!`;
+output.textContent = `LET'S PLAY!`;
 
 buttons.forEach(button => {button.addEventListener('click',()=>{
     let playerChoiceID = button.id;
+    if (playerChoiceID === "resetBtn"){
+        j=0;
+        i=0;
+        output.textContent = `LET'S PLAY!`;
+        player.textContent = `Player Score: ${i}`;
+        computer.textContent = `Computer Score: ${j}`;
+    }
+    else {
     if (playerChoiceID === "rock") playerSelection = 0;
     if (playerChoiceID === "paper") playerSelection = 1;
     if (playerChoiceID === "scissors") playerSelection = 2;
     computerSelection = getComputerChoice();
-    console.log(playerSelection);
-    console.log(computerSelection);
-    game();
+    game();}
 })
 });
 
@@ -48,15 +54,21 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
+
 function game(){
-    playRound(playerSelection, computerSelection);
-    player.textContent = `Player Score: ${i}`;
-    computer.textContent = `Computer Score: ${j}`;
-    if (i==5){
-        output.textContent = `OH NO! YOU WON!!! YOU MUST HAVE CHEATED!`;
+    if(i<5 && j<5){
+        playRound(playerSelection, computerSelection);
+        player.textContent = `Player Score: ${i}`;
+        computer.textContent = `Computer Score: ${j}`;
+        if (i==5){
+            output.textContent = `OH NO! YOU WON THE GAME!!! YOU MUST HAVE CHEATED!`;
+        }
+        if (j==5){
+            output.textContent = `HAHAHAHA YOU LOST THE GAME! YOU STINK!`
+        }
     }
-    if (j==5){
-        output.textContent = `HAHAHAHA YOU LOST! YOU STINK!`
+    else {
+        output.textContent = `Play a New Game`;
     }
 }
 
